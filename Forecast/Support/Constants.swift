9cd1a83ac56bd3b9355324fcf61f3c63f949e111
Howard Edwards <howarde.edwards@gmail.com>
2018-06-23 00:16:10 -0500

    added cell template for forecast

--- Forecast/Support/Constants.swift
+++ Forecast/Support/Constants.swift
@@ -21,3 +21,11 @@ struct Colors {
 	static let textBlue = UIColor(hexString: "#2F91FF")
 	static let textOrange = UIColor(hexString: "#FF8847")
 }
+
+struct XibCells {
+	static let forecastCell = "ForecastTableViewCell"
+}
+
+struct CellIdentifiers {
+	static let forecastCell = "ForecastTableViewCell"
+}
