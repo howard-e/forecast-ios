9e4dd5539e78a8a316b7f1be09a354ae4095ae86
Howard Edwards <howarde.edwards@gmail.com>
2018-06-22 20:30:23 -0500

    Initial Commit

---
+++ Forecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecast
+//
+//  Created by Howard Edwards on 6/22/18.
+//  Copyright © 2018 Howard Edwards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
